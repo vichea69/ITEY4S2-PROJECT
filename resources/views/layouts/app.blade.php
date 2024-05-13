047bd04a3f74a377ef18c5aeea5aad6fca251419
Vichea69 <smart.vichea@gmail.com>
2024-05-14 00:41:00 +0700

    Change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Der Leng With Me</title>
+    <title>Der leng with me</title>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
